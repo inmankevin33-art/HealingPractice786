68122ed8565e77493a58f487a50f6fc777817efc
Inman Kevin <inmankevin33@gmail.com>
2026-01-26 22:32:28 +0000

    Create layout.tsx

---
+++ src/app/birmingham/sexual-rejuvenation/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function BirminghamSexualRejuvenationLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="birmingham-sexual-wellness-wrapper">
+      {/* Structural container for Birmingham sexual health pages */}
+      {children}
+    </div>
+  );
+}
