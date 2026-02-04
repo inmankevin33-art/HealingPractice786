0e0e79d1998a2bc72ce6933775eae0022fb0a814
Inman Kevin <inmankevin33@gmail.com>
2026-02-04 15:02:06 +0000

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,8 @@
 // src/app/blog/[slug]/page.tsx
-import BlogClient from "@/components/pages/BlogPostClient";
+import BlogPostClient from "@/components/pages/BlogPostClient";
 
 export default function Page({ params }: { params: { slug: string } }) {
   // All the "Keyword" value comes from the content 
   // rendered by this component and the layout's metadata.
-  return <BlogClient slug={params.slug} />;
+  return <BlogPostClient slug={params.slug} />;
 }
