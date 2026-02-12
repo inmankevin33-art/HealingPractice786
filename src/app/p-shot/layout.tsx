eb2aae0cf9b8a10d5cef26f436441f7f27bfcb72
Inman Kevin <inmankevin33@gmail.com>
2026-02-12 18:49:04 +0000

    Update layout.tsx

--- src/app/p-shot/layout.tsx
+++ src/app/p-shot/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: {
@@ -8,6 +9,6 @@ export const metadata: Metadata = {
   description: "Official P-Shot® (Priapus Shot) provider serving Luton, St Albans & Hertfordshire.",
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
