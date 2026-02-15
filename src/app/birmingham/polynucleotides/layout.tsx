75e5fe2a42efd33f5cbe3e95208c1ea7ffefec26
Inman Kevin <inmankevin33@gmail.com>
2026-02-15 02:04:41 +0000

    Create layout.tsx

---
+++ src/app/birmingham/polynucleotides/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export default function BirminghamPolynucleotidesLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
