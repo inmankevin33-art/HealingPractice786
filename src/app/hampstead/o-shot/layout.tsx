9e1ff73d7aeaf2eeb21c75ccdb8c6942eeb695e8
Inman Kevin <inmankevin33@gmail.com>
2026-06-11 00:45:52 +0100

    Create layout.tsx

---
+++ src/app/hampstead/o-shot/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Hampstead Clinic",
+    default: "Doctor-Led O-Shot Treatment | Hampstead Clinic",
+  },
+  description: "Private doctor-led O-Shot treatment serving Hampstead, Belsize Park, West Hampstead & North West London.",
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
