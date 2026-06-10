7ee0966db6a71f697a5dac26ef02c889826fe7e2
Inman Kevin <inmankevin33@gmail.com>
2026-06-10 12:57:42 +0100

    Update address details in page.tsx

--- src/app/hampstead/p-shot/page.tsx
+++ src/app/hampstead/p-shot/page.tsx
@@ -78,8 +78,10 @@ const pShotSchemaHampstead = {
       "telephone": "+447990364147",
       "address": {
         "@type": "PostalAddress",
+        "streetAddress": "2 Hampstead High St",
         "addressLocality": "London",
         "addressRegion": "Greater London",
+        "postalCode": "NW3 1PR",
         "addressCountry": "GB"
       },
       "areaServed": [
